9615fefc041145440bd0cd4213f06036a5fc0674
Navid60-developer <navidtaj60@gmail.com>
2025-02-23 21:39:24 +0000

    Update main.js

--- Fashion/jsss/main.js
+++ Fashion/jsss/main.js
@@ -203,15 +203,19 @@ function includeHTML(el, file) {
 document.addEventListener("DOMContentLoaded", function () {
     // let basePath = window.location.pathname.includes("/fashion-template/") ? "/fashion-template/Fashion/" : "/Fashion/";
     // let basePath = window.location.origin.includes("github.io") ? "/fashion-template/Fashion/" : "/Fashion/";
-    let basePath = window.location.hostname.includes("github.io") ? "/fashion-template/Fashion/" : "/";
+    // let basePath = window.location.hostname.includes("github.io") ? "/fashion-template/Fashion/" : "/";
 
-    includeHTML(document.querySelector("head"), basePath + "toppe.html");
-    includeHTML(document.querySelector("header"), basePath + "menuue.html");
-    includeHTML(document.querySelector("footer"), basePath + "Footert.html");
+    // includeHTML(document.querySelector("head"), basePath + "toppe.html");
+    // includeHTML(document.querySelector("header"), basePath + "menuue.html");
+    // includeHTML(document.querySelector("footer"), basePath + "Footert.html");
     
     // includeHTML(document.querySelector("head"), "/fashion-template/Fashion/toppe.html");
     // includeHTML(document.querySelector("header"), "/fashion-template/Fashion/menuue.html");
     // includeHTML(document.querySelector("footer"), "/fashion-template/Fashion/Footert.html");
+    includeHTML(document.querySelector("header"), location.origin + "/toppe.html");
+    includeHTML(document.querySelector("header"), location.origin + "/menuue.html");
+    includeHTML(document.querySelector("header"), location.origin + "/Footert.html");
+
 
     // استفاده از MutationObserver برای ثبت دکمه‌ها
     const observer = new MutationObserver(function (mutations) {
