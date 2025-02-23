7b938edc433ff6394b604ed32e20a99b0565043d
Navid60-developer <navidtaj60@gmail.com>
2025-02-23 20:00:01 +0000

    Update main.js

--- Fashion/jsss/main.js
+++ Fashion/jsss/main.js
@@ -201,9 +201,15 @@ function includeHTML(el, file) {
 }
 
 document.addEventListener("DOMContentLoaded", function () {
-    includeHTML(document.querySelector("head"), "/fashion-template/Fashion/toppe.html");
-    includeHTML(document.querySelector("header"), "/fashion-template/Fashion/menuue.html");
-    includeHTML(document.querySelector("footer"), "/fashion-template/Fashion/Footert.html");
+    let basePath = window.location.pathname.includes("/fashion-template/") ? "/fashion-template/Fashion/" : "/Fashion/";
+
+    includeHTML(document.querySelector("head"), basePath + "toppe.html");
+    includeHTML(document.querySelector("header"), basePath + "menuue.html");
+    includeHTML(document.querySelector("footer"), basePath + "Footert.html");
+    
+    // includeHTML(document.querySelector("head"), "/fashion-template/Fashion/toppe.html");
+    // includeHTML(document.querySelector("header"), "/fashion-template/Fashion/menuue.html");
+    // includeHTML(document.querySelector("footer"), "/fashion-template/Fashion/Footert.html");
 
     // استفاده از MutationObserver برای ثبت دکمه‌ها
     const observer = new MutationObserver(function (mutations) {
