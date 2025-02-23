10f23875b5c9844e4143e147a688ffd067a85df7
Navid60-developer <navidtaj60@gmail.com>
2025-02-23 20:26:08 +0000

    Update main.js

--- Fashion/jsss/main.js
+++ Fashion/jsss/main.js
@@ -202,8 +202,8 @@ function includeHTML(el, file) {
 
 document.addEventListener("DOMContentLoaded", function () {
     // let basePath = window.location.pathname.includes("/fashion-template/") ? "/fashion-template/Fashion/" : "/Fashion/";
-    let basePath = window.location.origin.includes("github.io") ? "/fashion-template/Fashion/" : "/Fashion/";
-
+    // let basePath = window.location.origin.includes("github.io") ? "/fashion-template/Fashion/" : "/Fashion/";
+    let basePath = window.location.hostname.includes("github.io") ? "/fashion-template/Fashion/" : "/";
 
     includeHTML(document.querySelector("head"), basePath + "toppe.html");
     includeHTML(document.querySelector("header"), basePath + "menuue.html");
